726f730ebebcf15d9b2be009e878e4ddfcb05146
Wellien <jbm74700@yahoo.fr>
2019-09-06 10:37:50 +0200

    Update Inscription_Controleur.php

--- Inscription_Controleur.php
+++ Inscription_Controleur.php
@@ -1,4 +1,12 @@
 <?php
 	$Name = $_POST['Name'];
+	$FirstName = $_POST['FirstName'];
+	$Login = $_POST['Login'];
+	$Pass = $_POST['Pass'];
+
+	echo $Name;
+	echo $FirstName;
+	echo $Login;
+	echo $Pass;
 ?>
 
