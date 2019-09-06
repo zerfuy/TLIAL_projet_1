93127a83632c099c22718a8f0e718db32ce10a87
Wellien <jbm74700@yahoo.fr>
2019-09-06 08:26:06 +0200

    Inscription.php

---
+++ Incription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="UTF-8">
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<title>Incription</title>
+	</head>
+	<body>
+
+		<div id="Sidenav1" class="sidenav">
+ 			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+			<a href="./index.php" id="linkAccueil">Accueil</a>
+		  	<a href="./Connexion.php" id="linkConnexion">Connexion</a>
+		  	<a href="./Informations.php" id="linkInformations">Informations</a> 
+		</div>
+
+		<main id="main">
+  		<div id="menubar">
+    		<span  id="openMenuBtn" onclick="openNav()">&#9776; Menu</span>
+  		</div>
+	</body>
+</html>
