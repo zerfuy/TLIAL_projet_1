a52e33cf23e7e7d26363e19b85ace5d8b939301e
Wellien <jbm74700@yahoo.fr>
2019-09-12 16:23:13 +0200

    Update

--- Inscriptions/Inscription_Controleur.php
+++ Inscriptions/Inscription_Controleur.php
@@ -1,7 +1,7 @@
 <?php
 
 	include('Modele/Inscription_Connexion_BDD.php');
-	//include('Modele/Inscription_Check.php');
+	include('Modele/Inscription_Check.php');
 	include('Modele/Inscription_BDD.php');
 	$Name = $_POST['Name'];
 	$FirstName = $_POST['FirstName'];
@@ -9,10 +9,8 @@
 	$Pass = $_POST['Pass'];
 
 	$PDO = Connexion_BDD();
-	//$DATA = Check_User($PDO);
-	ADD_User($PDO,$Login,$Name,$FirstName,$Pass);
-	//$sth = $PDO->prepare('INSERT INTO USERS VALUES ("0", "test_login", "test", "test", "test")');
-	//$res = $sth -> execute();
+	$USER = ADD_User($PDO,$Login,$Name,$FirstName,$Pass);
+	print_r($USER);
 
 ?>
 
--- Inscriptions/Modele/Inscription_Check.php
+++ Inscriptions/Modele/Inscription_Check.php
@@ -1,8 +1,8 @@
 <?php
 	Function Check_User($PDO, $login)
 	{
-		$sth = $PDO->prepare("SELECT id_User FROM USERR WHERE Login = :valeur");
-		$sth ->( binValue(":valeur",$login,PDO::PARAM_STR)
+		$sth = $PDO->prepare("SELECT id_User FROM USERS WHERE Login = :valeur");
+		$sth -> (binValue(":valeur", $login, PDO::PARAM_STR);
 		$res = $sth -> execute();
 
 		if($res == TRUE)
